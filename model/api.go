f04b371295c08e451ef60099a19d792d6f06286b
abhijitacharya793 <abhijitacharya793@gmail.com>
2024-10-23 01:53:34 +0530

    added model

---
+++ model/api.go
@@ -0,0 +1,49 @@
+package model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Api struct {
+	ApiID           uuid.UUID    `json:"api_id"`
+	CreatedAt       *time.Time   `json:"created_at"`
+	UpdatedAt       *time.Time   `json:"updated_at"`
+	Headers         []Header     `json:"headers"`
+	QueryParams     []QueryParam `json:"query_params"`
+	PathParams      []PathParam  `json:"path_params"`
+	Target          string       `json:"target"`
+	RootDomain      string       `json:"root_domain"`
+	Domain          string       `json:"domain"`
+	Protocol        string       `json:"protocol"`
+	ProtocolVersion string       `json:"protocol_version"`
+	Port            string       `json:"port"`
+	Method          string       `json:"method"`
+	Path            string       `json:"path"`
+	Body            string       `json:"body"`
+}
+
+type Header struct {
+	HeaderID  uuid.UUID  `json:"header_id"`
+	CreatedAt *time.Time `json:"created_at"`
+	UpdatedAt *time.Time `json:"updated_at"`
+	Name      string     `json:"name"`
+	Value     string     `json:"value"`
+}
+
+type QueryParam struct {
+	QueryParamID uuid.UUID  `json:"query_param_id"`
+	CreatedAt    *time.Time `json:"created_at"`
+	UpdatedAt    *time.Time `json:"updated_at"`
+	Name         string     `json:"name"`
+	Value        string     `json:"value"`
+}
+
+type PathParam struct {
+	PathParamID uuid.UUID  `json:"path_param_id"`
+	CreatedAt   *time.Time `json:"created_at"`
+	UpdatedAt   *time.Time `json:"updated_at"`
+	Name        string     `json:"name"`
+	Value       string     `json:"value"`
+}
